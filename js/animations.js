de43efcef90c6dfcef027913c075542e1ab4c6d8
Cesar Azambuja <cesarazambuja@MacBook-Pro-de-Cesar.local>
2025-04-25 10:02:07 -0300

    add animation aos

---
+++ js/animations.js
@@ -0,0 +1,10 @@
+
+document.addEventListener("DOMContentLoaded", function () {
+  AOS.init({
+    duration: 900,
+    once: true,
+    offset: 100,
+    easing: "ease-in-out",
+    delay: 100,
+  });
+});
